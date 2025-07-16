778f1c5104b568fdfde8ac3f0226b989a49094ef
Christian Nuss <christian@ybor.ai>
2025-07-16 17:38:27 -0400

    refactor

--- 0-ede/nix/rootfs/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Entrypoint script for nix develop environment
-
-# Source nix profile
-source ~/.nix-profile/etc/profile.d/nix.sh
-
-# Execute command in nix develop environment
-exec nix develop /ede --command "$@"
