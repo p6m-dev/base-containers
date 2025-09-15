f82b2216e58394a77dee68977acf02bdcb0e1d93
Christian Nuss <christian@ybor.ai>
2025-09-15 12:43:31 -0400

    re-enable extension install

--- 1-ede/vscode/extensions-github.sh
+++ 1-ede/vscode/extensions-github.sh
@@ -13,7 +13,7 @@ echo "EDE-VScode Latest release: $EDE_VSCODE_RELEASE"
 
 curl -L -o /tmp/ede-vscode.vsix "https://github.com/ybor-studio/ede-vscode/releases/download/$EDE_VSCODE_RELEASE/ede-vscode-$EDE_VSCODE_RELEASE.vsix"
 
-# echo "Installing EDE-VScode extension on ${IDE}..."
-# $IDE --extensions-dir=/${IDE}/extensions --install-extension /tmp/ede-vscode.vsix --force
+echo "Installing EDE-VScode extension on ${IDE}..."
+$IDE --extensions-dir=/${IDE}/extensions --install-extension /tmp/ede-vscode.vsix --force
 
 echo "EDE-VScode extension installed successfully."
